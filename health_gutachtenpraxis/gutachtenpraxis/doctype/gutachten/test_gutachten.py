5053cd04ca69bd8cf207388569c9187c6cf7a7fc
Aron Wiederkehr <aron.wiederkehr@gmai.com>
2023-05-24 17:05:46 +0200

    added module and essential doctypes

---
+++ health_gutachtenpraxis/gutachtenpraxis/doctype/gutachten/test_gutachten.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Aron Wiederkehr and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestGutachten(FrappeTestCase):
+	pass
