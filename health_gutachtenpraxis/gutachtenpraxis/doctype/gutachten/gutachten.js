f2b6f4a755450acad29cb1fa90be160ce31114f2
Aron Wiederkehr <aron.wiederkehr@gmai.com>
2023-06-06 14:24:53 +0000

    generate pdf, no extension

--- health_gutachtenpraxis/gutachtenpraxis/doctype/gutachten/gutachten.js
+++ health_gutachtenpraxis/gutachtenpraxis/doctype/gutachten/gutachten.js
@@ -1,34 +1,54 @@
 // Copyright (c) 2023, Aron Wiederkehr and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Gutachten', {
-	court: function(frm) {
-        if (frm.doc.court) {
-            frappe.call({
-                method: "health_gutachtenpraxis.gutachtenpraxis.utils.get_judges",
-                args: {
-                    "court": frm.doc.court
-                },
-                callback: function(r) {
-                    if (r.message) {
-                        // Store the judge names in a separate variable
-                        let judgeNames = r.message.map(judge => judge.name);
-                        
-                        // Set the "options" property to "Richter"
-                        frm.set_df_property("judge", "options", "Richter");
-                        
-                        // Filter the options that are displayed to the user
-                        frm.set_query("judge", function() {
-                            return {
-                                filters: {"name": ["in", judgeNames]}
-                            };
-                        });
-                    }
-                }
+frappe.ui.form.on("Gutachten", {
+  refresh: function (frm) {
+    frm.add_custom_button("Generate PDF", function () {
+      frappe.call({
+        method:
+          "health_gutachtenpraxis.gutachtenpraxis.doctype.gutachten.gutachten.generate_pdf",
+        args: {
+          doctype: frm.doc.doctype,
+          name: frm.doc.name,
+        },
+        callback: function (r) {
+          if (r.message) {
+            // Open the PDF in a new tab
+            window.open(r.message);
+          }
+        },
+      });
+    });
+  },
+  court: function (frm) {
+    if (frm.doc.court) {
+      frappe.call({
+        method: "health_gutachtenpraxis.gutachtenpraxis.utils.get_judges",
+        args: {
+          court: frm.doc.court,
+        },
+        callback: function (r) {
+          if (r.message) {
+            // Store the judge names in a separate variable
+            let judgeNames = r.message.map((judge) => judge.name);
+
+            // Set the "options" property to "Richter"
+            frm.set_df_property("judge", "options", "Richter");
+
+            // Filter the options that are displayed to the user
+            frm.set_query("judge", function () {
+              return {
+                filters: { name: ["in", judgeNames] },
+              };
             });
-        } else {
-            frm.set_df_property("judge", "options", []);
-            frm.set_query("judge", function() { return; });
-        }
+          }
+        },
+      });
+    } else {
+      frm.set_df_property("judge", "options", []);
+      frm.set_query("judge", function () {
+        return;
+      });
     }
+  },
 });
--- health_gutachtenpraxis/gutachtenpraxis/doctype/gutachten/gutachten.py
+++ health_gutachtenpraxis/gutachtenpraxis/doctype/gutachten/gutachten.py
@@ -18,3 +18,30 @@ class Gutachten(Document):
 			self.due_date = new_date.strftime("%Y-%m-%d")
 		else:
 			frappe.throw("Bitte Eingangsdatum angeben!")
+
+	
+@frappe.whitelist()
+def generate_pdf(doctype, name):
+	# Get the default Letter Head
+	options = {
+        "format": "A4",
+        "orientation": "Portrait"
+    }
+
+	# Generate the HTML for the Print Format
+	html = frappe.get_print(doctype, name, print_format="Anamnesebogen")
+
+	# Create the PDF
+	pdf = frappe.utils.pdf.get_pdf(html)
+
+	# Save the PDF to a file
+	file = frappe.get_doc({
+		"doctype": "File",
+		"file_name": f"{name}.pdf",  # Add .pdf extension to the file name
+		"content": pdf,
+		"is_private": 1
+	})
+	file.save()
+
+	return file.file_url
+
