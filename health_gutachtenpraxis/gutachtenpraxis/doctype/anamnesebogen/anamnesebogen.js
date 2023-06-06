08bd60d3231666fea5343f6998e91d77c1f9f9c3
Aron Wiederkehr <aron.wiederkehr@gmai.com>
2023-06-06 15:48:49 +0000

    fixed shitty anamnese bug fest

--- health_gutachtenpraxis/gutachtenpraxis/doctype/anamnesebogen/anamnesebogen.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2023, Aron Wiederkehr and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Anamnesebogen', {
-	// refresh: function(frm) {
-
-	// }
-});
--- health_gutachtenpraxis/gutachtenpraxis/doctype/anamnesebogen/anamnesebogen.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2023, Aron Wiederkehr and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class Anamnesebogen(Document):
-	pass
