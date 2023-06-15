22a6d5151330d17b7a8b07921b18367eb2b5746a
Aron Wiederkehr <aron.wiederkehr@gmai.com>
2023-06-15 13:39:20 +0000

    added dob to naming series

--- health_gutachtenpraxis/gutachtenpraxis/doctype/gutachten_patient/gutachten_patient.py
+++ health_gutachtenpraxis/gutachtenpraxis/doctype/gutachten_patient/gutachten_patient.py
@@ -3,14 +3,26 @@
 
 # import frappe
 from frappe.model.document import Document
+from frappe.model.naming import getseries
 from frappe.contacts.address_and_contact import load_address_and_contact
-from frappe.utils import getdate
+from frappe.utils import getdate, formatdate
 
 
 class GutachtenPatient(Document):
 	def onload(self):
 		"""Load address and contacts in `__onload`"""
 		load_address_and_contact(self)
+
+	def autoname(self):
+		# Format date_of_birth as DD.MM.YYYY
+		formatted_date = formatdate(self.dob, "dd.MM.yyyy")
+
+		# Concatenate first_name, last_name, and formatted_date
+		name = f"{self.first_name} {self.last_name}, {formatted_date}"
+
+		self.name = name
+		
+
 
 	def validate(self):
 		self.set_full_name()
