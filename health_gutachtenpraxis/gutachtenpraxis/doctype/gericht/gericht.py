99153b5697643abedb7a7b995211ebd7b75db204
Aron Wiederkehr <aron.wiederkehr@gmai.com>
2023-06-16 16:50:00 +0000

    Added Kanban Board view + Extras

--- health_gutachtenpraxis/gutachtenpraxis/doctype/gericht/gericht.py
+++ health_gutachtenpraxis/gutachtenpraxis/doctype/gericht/gericht.py
@@ -1,8 +1,13 @@
 # Copyright (c) 2023, Aron Wiederkehr and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Gericht(Document):
-	pass
+	def after_insert(self):
+		kanban_board = frappe.get_doc('Kanban Board', 'Gutachten Board')
+		kanban_board.append('columns', {
+            'column_name': self.name
+        })
+		kanban_board.save()
---
+++ health_gutachtenpraxis/patches/setup_kanban_board.py
@@ -0,0 +1,32 @@
+import frappe
+
+def execute():
+    # Fetch all Amtsgericht documents
+    courts = frappe.get_all('Gericht')
+    # Create Kanban Board with each Amtsgericht as a column
+    kanban_board = frappe.get_doc({
+        "doctype": "Kanban Board",
+        "kanban_board_name": "Gutachten Board",
+        "reference_doctype": "Gutachten",
+        "field_name": "court",
+        "card_fields": ["name"]
+    })
+    print(kanban_board.as_dict())
+    print("Add each Amtsgericht as a column")
+    # Add each Amtsgericht as a column
+    for court in courts:
+        print(court.name)
+        kanban_board.append("columns", {
+            "column_name": court.name
+        })
+    print(kanban_board.as_dict())
+    print("Save the Kanban Board")
+    # Save the Kanban Board
+    kanban_board.insert()
+    # print("Fetch all Gutachten documents")
+    # # Fetch all Gutachten documents
+    # gutachtens = frappe.get_all('Gutachten')
+
+    # # Map each Gutachten to the appropriate column
+    # for gutachten in gutachtens:
+    #     frappe.db.set_value('Gutachten', gutachten.name, 'kanban_column', gutachten.court)
