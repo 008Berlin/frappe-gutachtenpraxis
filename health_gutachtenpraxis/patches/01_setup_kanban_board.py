3dbdab0ec72c9d71ef47049c6202bbfde09cc909
aronjanosch <aron.wiederkehr@gmail.com>
2023-08-30 18:15:23 +0000

    small fix

--- health_gutachtenpraxis/patches/01_setup_kanban_board.py
+++ health_gutachtenpraxis/patches/01_setup_kanban_board.py
@@ -1,5 +1,3 @@
-import frappe
-
 def execute():
     # Fetch all Amtsgericht documents
     courts = frappe.get_all('Gericht')
@@ -30,19 +28,6 @@ def execute():
         kanban_board.insert()
         
     else:
-        kanban_board = frappe.get_doc('Kanban Board', 'Gutachten Board')
-        existing_columns = [column.column_name for column in kanban_board.columns]
-        
-        print("Add missing Amtsgerichts as columns")
-        
-        for court in courts:
-            if court.name not in existing_columns:
-                print(f"Adding column for {court.name}")
-                kanban_board.append("columns", {
-                    "column_name": court.name
-                })
-        
-        kanban_board.save()
-        print("Kanban Board updated.")
+        print("Kanban Board already exists. Skipping creation.")
         
     print("Patch execution completed.")
