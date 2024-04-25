9c88db1b15d5fe4c7a542b6eb8fa9f49a4a4e44f
D0rianW <62951927+D0rianW@users.noreply.github.com>
2024-04-25 09:33:35 +0200

    Farben der Termin-Liste angepasst

--- health_gutachtenpraxis/gutachtenpraxis/doctype/termin/termin_list.js
+++ health_gutachtenpraxis/gutachtenpraxis/doctype/termin/termin_list.js
@@ -2,16 +2,16 @@ frappe.listview_settings['Termin'] = {
     //filters: [["status", "=", "Open"]],
     get_indicator: function (doc) {
         var colors = {
-            "Wiedervorlage": "orange",
-            "Privat Fichtel - Praxis": "yellow",
-            "Termin bestätigt, Gutachten": "blue",
-            "Planung Fahrer": "green",
-            "Vergebliche Anfahrt": "orange",
-            "abgs. Praxis - neu terminieren": "red",
-            "Termin mitgeteilt, nicht bestätigt": "red",
-            "Termin geplant nicht mitgeteilt": "red",
-            "Urlaub": "red",
-            "Arbeitszeiten (An- und Abwesenheit)": "red"
+            "Wiedervorlage": "purple",
+            "Privat Fichtel - Praxis": "black",
+            "Termin bestätigt, Gutachten": "red",
+            "Planung Fahrer": "cyan",
+            "Vergebliche Anfahrt": "gray",
+            "abgs. Praxis - neu terminieren": "orange",
+            "Termin mitgeteilt, nicht bestätigt": "green",
+            "Termin geplant nicht mitgeteilt": "cyan",
+            "Urlaub": "cyan",
+            "Arbeitszeiten (An- und Abwesenheit)": "purple"
 
             /*
             "Wiedervorlage": "orange",
