b22d6eae2250644bbf380289947ddcc8f3ed7b71
D0rianW <62951927+D0rianW@users.noreply.github.com>
2024-04-30 10:57:13 +0200

    Update tagesliste.js

--- health_gutachtenpraxis/gutachtenpraxis/doctype/tagesliste/tagesliste.js
+++ health_gutachtenpraxis/gutachtenpraxis/doctype/tagesliste/tagesliste.js
@@ -1,3 +1,6 @@
+// Copyright (c) 2023, Aron Wiederkehr and contributors
+// For license information, please see license.txt
+
 frappe.ui.form.on('Tagesliste', {
     onload: function (frm) {
         refreshMap(frm, createMap);
