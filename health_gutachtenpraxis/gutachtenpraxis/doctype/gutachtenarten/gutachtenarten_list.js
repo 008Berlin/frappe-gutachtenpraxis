21ebebfebb30a5773846def743cf79aba0d07ffd
D0rianW <62951927+D0rianW@users.noreply.github.com>
2024-04-30 10:46:34 +0200

    Update gutachtenarten_list.js

--- health_gutachtenpraxis/gutachtenpraxis/doctype/gutachtenarten/gutachtenarten_list.js
+++ health_gutachtenpraxis/gutachtenpraxis/doctype/gutachtenarten/gutachtenarten_list.js
@@ -1,3 +1,6 @@
+// Copyright (c) 2023, Aron Wiederkehr and contributors
+// For license information, please see license.txt
+
 frappe.listview_settings['Gutachtenarten'] = {
     hide_name_column: true,
     colwidths: { "appraisal_type": 300 },
