22ad0a40edfc94df3ce5be3e09c132b6a2b2d373
D0rianW <62951927+D0rianW@users.noreply.github.com>
2024-04-30 10:57:41 +0200

    Update tagesliste_calendar.js

--- health_gutachtenpraxis/gutachtenpraxis/doctype/tagesliste/tagesliste_calendar.js
+++ health_gutachtenpraxis/gutachtenpraxis/doctype/tagesliste/tagesliste_calendar.js
@@ -1,3 +1,6 @@
+// Copyright (c) 2023, Aron Wiederkehr and contributors
+// For license information, please see license.txt
+
 frappe.views.calendar['Tagesliste'] = {
     field_map: {
         start: 'date',
